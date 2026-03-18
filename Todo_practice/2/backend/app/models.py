49359471548648839bf6ecf2831d59e2a5bb9274
Hamza Asghar <hamzaraftar765@gmail.com>
2026-03-18 21:03:55 +0500

    add serializer and models

--- Todo_practice/2/backend/app/models.py
+++ Todo_practice/2/backend/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
